b95566343a71e358de9d8db0600d6657f5fc9420
Omar_Sanchez <ols@andrew.cmu.edu>
2023-10-30 19:58:36 -0400

    Add 3x2 Confusion matrix

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -35,3 +35,37 @@ def evaluate_classification(
 
     accuracy = sum(correct) / len(correct)
     return correct, accuracy
+
+
+def get_confusion_matrix(predict, original, unknown_value="unknown"):
+    """Get the confusion matrix of the results
+    Outputs a 3x2 matrix categorizing the results into correct_numerical_answer, incorrect_numerical_answer, and unknown
+    Args:
+        predict: The predicted results, they can be a number or unknown
+        original: The original results, they can be a number or unknown
+    Returns
+        A 3x2 matrix of the results"""
+    confusion_matrix = np.zeros((3, 2))
+    none_values = 0
+    for i in range(len(predict)):
+        if predict[i] is None:
+            none_values += 1
+        elif original[i] == predict[i]:
+            if predict[i] == unknown_value:
+                # correct unknown
+                confusion_matrix[1, 1] += 1
+            else:
+                # correct number
+                confusion_matrix[0, 0] += 1
+        elif original[i] != predict[i]:
+            if predict[i] == unknown_value:
+                # incorrect unknown it was a number
+                confusion_matrix[1, 0] += 1
+            elif original[i] == unknown_value:
+                # incorrect number it was unknown
+                confusion_matrix[2, 1] += 1
+            else:
+                # both numbers but not equal
+                confusion_matrix[2, 0] += 1
+
+    return confusion_matrix, none_values
